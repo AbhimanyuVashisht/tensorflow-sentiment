ca94b65e8c913ae2582e3901bf7a658403f85715
WillNotFit <abhimanyuvashisht.av@gmail.com>
2018-03-18 18:45:01 +0530

    CNN

--- lstm_sequence_classification.py
+++ lstm_sequence_classification.py
@@ -1,8 +1,8 @@
 import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, LSTM
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
@@ -20,6 +20,8 @@ X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 embedding_vector_length = 32
 model = Sequential()
 model.add(Embedding(top_words, embedding_vector_length, input_lenght=max_review_length))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
 model.add(LSTM(100))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -27,4 +29,5 @@ print(model.summary())
 model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=3, batch_size=64)
 
 scores = model.evaluate(X_test, Y_test, verbose=0)
-print("Accuracy: %.2%%" %(scores[1]*100))
+print("Accuracy: %ds.2%%" % (scores[1] * 100))
+
